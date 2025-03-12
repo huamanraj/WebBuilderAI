426152ee65003f2b2828090ce266bc57d7ac65eb
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-03-13 03:01:51 +0530

    url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://web-builder-ai-backend.vercel.app/',
         changeOrigin: true,
       },
     },
